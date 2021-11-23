a5b00c0ac667160119891fb6889b79b5addf3b13
Gavi Galloway <gsgalloway@users.noreply.github.com>
2021-11-23 23:19:03 +0000

    Use safe-service-client instead of builtin safe-sdk (delegatecall not supported)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
-import { Button, Card, Divider, Loader, Title, Text } from '@gnosis.pm/safe-react-components'
+import { Button, Card, Divider, Loader, Title } from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
-import OpenseaBulkPurchaser from '@gsgalloway/opensea-bulk-purchaser';
-import { BigNumber, getDefaultProvider } from 'ethers';
+import OpenseaBulkPurchaser, { SinglePurchaseTx } from '@gsgalloway/opensea-bulk-purchaser';
+import { BigNumber, providers, Contract } from 'ethers';
 import NFTForm from './NFTForm';
 import ManageListModal from './ManageListModal';
 import TokenList from './TokenList';
 import { TokenDescription } from './types';
-import NetworkSelect from './NetworkSelect';
-import { Network } from 'opensea-js';
+import { Network as OpenseaNetwork } from 'opensea-js';
+import { signHash, calculateSafeTransactionHash } from "@gnosis.pm/safe-contracts";
+import { gnosisSafeABI } from './gnosis-abi';
+import SafeServiceClient from '@gnosis.pm/safe-service-client'
+import EthSafeTransaction from '@gnosis.pm/safe-core-sdk/dist/src/utils/transactions/SafeTransaction';
+import { EthSignSignature } from '@gnosis.pm/safe-core-sdk';
+import { Provider, Network, Web3Provider } from '@ethersproject/providers';
+import { useQuery } from 'react-query';
 
 const Container = styled.div`
   padding: 1rem;
@@ -21,18 +27,23 @@ const Container = styled.div`
   flex-direction: column;
 `
 
+const loadNetwork = async (provider: Provider): Promise<Network> => {
+  return await provider.getNetwork();
+}
 
-const SafeApp = (): React.ReactElement => {
-  const { sdk, safe } = useSafeAppsSDK()
-  const [isOpen, setIsOpen] = useState(false);
-  const [tokens, setTokens] = useState<TokenDescription[]>([])
-  const [inputTokenID, setInputTokenID] = useState('');
-  const [inputTokenContractAddress, setInputTokenContractAddress] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedNetwork, setSelectedNetwork] = useState('mainnet');
+const getProvider = (): Web3Provider => {
+  const provider = new providers.Web3Provider((window as any).ethereum)
+  if (!provider) {
+    throw new Error("no ethereum provider registered");
+  }
+  return provider;
+}
+
+const getOpenseaParams = (network: Network): {openseaApiKey: string, openseaNetworkName: OpenseaNetwork} => {
+  const networkName = network.name;
 
   let openseaApiKey: string;
-  if (selectedNetwork === "mainnet") {
+  if (networkName === "mainnet" || networkName === "homestead") {
     if (!process.env.REACT_APP_OPENSEA_API_KEY) {
       throw new Error("must define REACT_APP_OPENSEA_API_KEY");
     }
@@ -44,19 +55,110 @@ const SafeApp = (): React.ReactElement => {
     openseaApiKey = process.env.REACT_APP_RINKEBY_OPENSEA_API_KEY
   }
 
-  let openseaNetworkName: Network;
-  switch (selectedNetwork) {
+  let openseaNetworkName: OpenseaNetwork;
+  switch (networkName) {
     case "mainnet":
-      openseaNetworkName = Network.Main;
+    case "homestead":
+      openseaNetworkName = OpenseaNetwork.Main;
       break;
     case "rinkeby":
-      openseaNetworkName = Network.Rinkeby;
+      openseaNetworkName = OpenseaNetwork.Rinkeby;
       break;
     default:
       throw new Error(
-        `network ${selectedNetwork} is not supported by opensea-js`
+        `network ${networkName} is not supported by opensea-js`
       );
   }
+  return {
+    openseaApiKey,
+    openseaNetworkName,
+  }
+}
+
+const SafeApp = (): React.ReactElement => {
+  const { safe } = useSafeAppsSDK();
+  const [isOpen, setIsOpen] = useState(false);
+  const [tokens, setTokens] = useState<TokenDescription[]>([])
+  const [inputTokenID, setInputTokenID] = useState('');
+  const [inputTokenContractAddress, setInputTokenContractAddress] = useState('');
+  const [isTransactionBuilderLoading, setIsTransactionBuilderLoading] = useState(false);
+
+  const submitTx = useCallback(async () => {
+    try {
+      setIsTransactionBuilderLoading(true);
+      await (window as any).ethereum.enable();
+      const provider = getProvider();
+      const network = await provider.getNetwork();
+      const signer = provider.getSigner();
+
+      const {openseaApiKey, openseaNetworkName} = getOpenseaParams(network);
+      const openseaBulkPurchaser = new OpenseaBulkPurchaser(provider, {openseaApiKey, network: openseaNetworkName});
+      const purchaseTxs: SinglePurchaseTx[] = [];
+      for (const token of tokens) {
+        const purchaseTx = await openseaBulkPurchaser.createSingleTokenPurchase(token.id, token.contractAddress, safe.safeAddress);
+        purchaseTxs.push(purchaseTx);
+      }
+
+      const batchTx = await openseaBulkPurchaser.createBatchTransactionFromPurchases(purchaseTxs, safe.safeAddress);
+      const safeContract = new Contract(safe.safeAddress, gnosisSafeABI, provider);
+
+      const safeTxHash = calculateSafeTransactionHash(safeContract, batchTx, network.chainId);
+
+      const signature = await signHash(signer, safeTxHash);
+
+      let safeServiceURL = 'https://safe-transaction.gnosis.io';
+      if (network.name === 'rinkeby') {
+        safeServiceURL = 'https://safe-transaction.rinkeby.gnosis.io';
+      }
+      const safeService = new SafeServiceClient(safeServiceURL);
+
+      const ethSafeTx = new EthSafeTransaction({
+        baseGas: BigNumber.from(batchTx.baseGas).toNumber(),
+        data: batchTx.data,
+        gasPrice: BigNumber.from(batchTx.gasPrice).toNumber(),
+        gasToken: batchTx.gasToken,
+        nonce: BigNumber.from(batchTx.nonce).toNumber(),
+        operation: batchTx.operation,
+        refundReceiver: batchTx.refundReceiver,
+        safeTxGas: BigNumber.from(batchTx.safeTxGas).toNumber(),
+        to: batchTx.to,
+        value: BigNumber.from(batchTx.value).toString(),
+      })
+      ethSafeTx.addSignature(new EthSignSignature(signature.signer, signature.data));
+
+      await safeService.proposeTransaction({
+        safeAddress: safe.safeAddress,
+        safeTransaction: ethSafeTx,
+        senderAddress: await signer.getAddress(),
+        safeTxHash: safeTxHash,
+      })
+
+      alert("Transaction proposed! Please review and execute on the Transactions tab in the Gnosis UI.")
+    }
+    catch (e) {
+      console.error(e);
+      alert(e);
+    }
+    finally {
+      setIsTransactionBuilderLoading(false);
+    }
+  }, [tokens, safe])
+
+  const provider = getProvider();
+
+  const { isLoading: isNetworkLoading, data: network, error } = useQuery(['loadNetwork'], () => loadNetwork(provider))
+  if (isNetworkLoading) {
+      return <Loader size="lg" />
+  }
+  if (error) {
+      alert(error);
+      return <></>;
+  }
+  if (network === undefined) {
+      throw new Error("network is undefined")
+  }
+
+  const {openseaApiKey, openseaNetworkName} = getOpenseaParams(network);
 
   const onNFTFormSubmitted = () => {
     setTokens([...tokens, {id: BigNumber.from(inputTokenID), contractAddress: inputTokenContractAddress}])
@@ -85,32 +187,9 @@ const SafeApp = (): React.ReactElement => {
     })
   }
 
-  const submitTx = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const openseaBulkPurchaser = new OpenseaBulkPurchaser(getDefaultProvider(), {openseaApiKey, network: openseaNetworkName});
-      const purchaseTxs = await Promise.all(tokens.map(token => {
-        return openseaBulkPurchaser.createSingleTokenPurchase(token.id, token.contractAddress, safe.safeAddress)
-      }));
-
-      const batchTx = await openseaBulkPurchaser.createBatchTransactionFromPurchases(purchaseTxs, safe.safeAddress);
-      setIsLoading(false);
-      await sdk.txs.send({
-        txs: [{
-          to: batchTx.to,
-          value: BigNumber.from(batchTx.value).toString(),
-          data: batchTx.data,
-        }]
-      })
-    }
-    catch (e) {
-      console.error(e);
-    }
-  }, [tokens, safe, sdk, openseaNetworkName, openseaApiKey])
-
   return (
     <Container>
-      {isLoading && (
+      {isTransactionBuilderLoading && (
         <Loader size={"lg"} />
       )}
 
@@ -121,8 +200,6 @@ const SafeApp = (): React.ReactElement => {
           <TokenList tokens={tokens} network={openseaNetworkName} apiKey={openseaApiKey}/>
         </Card>
       )}
-
-      <Text size="lg">Network:</Text><NetworkSelect selectedNetwork={selectedNetwork} setSelectedNetwork={setSelectedNetwork} />
 
       <br />
 
